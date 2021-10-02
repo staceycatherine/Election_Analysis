c3cd7360d4cea1014dadbe3b8a4ff6f666468216
Staceycatherine <staceyhart1002@gmail.com>
2021-10-02 12:08:15 -0400

    practice

---
+++ Python_practice.py
@@ -0,0 +1,12 @@
+# counties= {"Arapahoe","Denver","Jefferson"}
+# print(counties)
+counties=["Arapahoe","Denver","Jefferson"]
+print(counties)
+if counties[1] == "Denver":
+    print(counties[1])
+    
+
+
+
+
+
